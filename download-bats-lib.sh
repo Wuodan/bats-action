f9b7d95b586b769cb86d93bad66ef35bf5f057dc
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-08 16:16:31 +0200

    Simplify Bats action installation

--- download-bats-lib.sh
+++ download-bats-lib.sh
@@ -5,14 +5,10 @@ repo="${1:?Usage: $0 <repo> <version> <tempdir>}"
 version="${2:?Usage: $0 <repo> <version> <tempdir>}"
 tempdir="${3:?Usage: $0 <repo> <version> <tempdir>}"
 
-declare -a curl_args=( --retry 4 --retry-connrefused )
-if [[ -n "${GITHUB_TOKEN:-}" ]]; then
-  curl_args+=( -H "Authorization: token $GITHUB_TOKEN" )
-fi
-
-url="https://api.github.com/repos/${repo}/tarball/v${version}"
+url="https://github.com/${repo}/archive/refs/tags/v${version}.tar.gz"
 echo "Downloading $url to $tempdir" >&2
 mkdir -p "$tempdir"
-curl -sL "${curl_args[@]}" "$url" \
+curl --fail --silent --show-error --location \
+  --retry 4 --retry-connrefused "$url" \
   | tar xz -C "$tempdir" --strip-components 1
 echo "${repo} v${version} downloaded to ${tempdir}" >&2
