49ce3198cec38d5a329224c65c2b7ca5713be348
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-09 02:30:03 +0200

    Install only Bats library runtime files

--- install-bats.sh
+++ install-bats.sh
@@ -11,16 +11,17 @@ mkdir -p "$install_path"
 
 install_library() {
   local repository="$1"
-  local source_directory="$2"
 
   mkdir -p "$install_path/$repository"
-  cp -R "$temp_dir/$repository/$source_directory/." \
+  cp -R "$temp_dir/$repository"/{load.bash,src} \
     "$install_path/$repository/"
 }
 
-install_library bats-support .
-install_library bats-assert .
-install_library bats-detik lib
-install_library bats-file .
+install_library bats-support
+install_library bats-assert
+install_library bats-file
+
+mkdir -p "$install_path/bats-detik"
+cp -R "$temp_dir/bats-detik/lib/." "$install_path/bats-detik/"
 
 rm -rf "$temp_dir"
