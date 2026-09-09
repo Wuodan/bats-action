28768f5317a5bfb4f83ad19e737e9c7b3158619f
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-09-09 02:34:13 +0200

    Unify Bats library installation

--- install-bats.sh
+++ install-bats.sh
@@ -13,15 +13,17 @@ install_library() {
   local repository="$1"
 
   mkdir -p "$install_path/$repository"
-  cp -R "$temp_dir/$repository"/{load.bash,src} \
-    "$install_path/$repository/"
+  if [ "$repository" = bats-detik ]; then
+    cp -R "$temp_dir/$repository/lib/." "$install_path/$repository/"
+  else
+    cp -R "$temp_dir/$repository"/{load.bash,src} \
+      "$install_path/$repository/"
+  fi
 }
 
 install_library bats-support
 install_library bats-assert
+install_library bats-detik
 install_library bats-file
-
-mkdir -p "$install_path/bats-detik"
-cp -R "$temp_dir/bats-detik/lib/." "$install_path/bats-detik/"
 
 rm -rf "$temp_dir"
